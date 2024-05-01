c4b62309261aa3384bb8ef92c340896ab0d81b66
unknown <apraiz.mikel@gmail.com>
2024-05-01 12:41:13 +0200

    Actulización de anotaciones

--- src/lib/AutoTranslateTextarea.jsx
+++ src/lib/AutoTranslateTextarea.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import { useNeskatila } from "./useNeskatila";
 import PropTypes from "prop-types";
 
+// Este un componente utiliza el Hook useNeskatila para traducir automaticamente el contenido que haya en un input.
 
 export const AutoTranslateTextArea = (props) => {
     const { sourceLanguage, targetLanguage } = props;
     const { translate } = useNeskatila();
     const [textAreaValue, setTextAreaValue] = useState('');
+
+
+     // "translate" proporcionado por el Hook useNeskatila para traducir el texto del idioma de origen al idioma de destino.
 
     const handleTranslate = async (event) => {
         try {
@@ -18,7 +22,12 @@ export const AutoTranslateTextArea = (props) => {
         }
     };
 
+        // El valor del input se actualiza con "handleChange". Este actualiza el estado "textAreaValue" del texto introducido.
+
     const handleChange = async (event) => {
+
+        // El resultado de la traduccion se guarda en el estado textAreaValue, que se muestra en el Input.
+
         setTextAreaValue(event.target.value);
     };
 
@@ -26,6 +35,9 @@ export const AutoTranslateTextArea = (props) => {
         <textarea value={textAreaValue} onChange={handleChange} onBlur={handleTranslate} placeholder="Escribe los textos de tu Web"></textarea>
     )
 }
+
+// PropTypes permite validar las props que se van pasar a los componentes, en este caso AutoTranslatedLabel, ya que esta destinado a ser reutilizados en otros codigos. De esta manera,
+// verificamos si las propiedades cumplen con los valores que queresmos para este componente. Nos sirve como una deteccion temprana de errores al recibir propiedades con valores incorrectos.
 
 AutoTranslateTextArea.propTypes = {
     sourceLanguage: PropTypes.string,
